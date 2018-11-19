cf7323b8819f5d008530308004277d16ccd57375
Filip Defar <filip.defar@toptal.com>
2018-11-19 12:10:44 +0100

    Flatten the array before rendering

--- src/renderGrid.ts
+++ src/renderGrid.ts
@@ -7,6 +7,17 @@ type dataRow = divisionDataset[];
 const width = 1000;
 const height = 1000;
 
+const blockSize = 24;
+const blockPadding = 5;
+
+type Datum = {
+  fill: string;
+  offsetX: number;
+  offsetY: number;
+  x: number;
+  y: number;
+};
+
 export function renderGrid(
   parent: Selection<BaseType, any, any, any>,
   data: dataRow[]
@@ -16,14 +27,34 @@ export function renderGrid(
   const cellWidth = width / columns;
   const cellHeight = height / rows;
 
+  const newDataset: Datum[] = [];
   data.forEach((row, rowIndex) => {
     row.forEach((divisionData, columnIndex) => {
-      const divisionGroup = parent.append('g');
-      divisionGroup.attr(
-        'transform',
-        `translate(${cellWidth * columnIndex},${cellHeight * rowIndex})`
-      );
-      renderDivision(divisionGroup, divisionData);
+      const columns = Math.ceil(Math.sqrt(divisionData.length));
+
+      divisionData.forEach((d, groupIndex) => {
+        newDataset.push({
+          fill: d,
+          offsetX: cellWidth * columnIndex,
+          offsetY: cellHeight * rowIndex,
+          x: (groupIndex % columns) * (blockSize + blockPadding),
+          y: Math.floor(groupIndex / columns) * (blockSize + blockPadding)
+        });
+      });
     });
   });
+
+  const blocksSelection = parent
+    .selectAll('rect.divisionBlock')
+    .data(newDataset);
+
+  blocksSelection
+    .enter()
+    .append('rect')
+    .attr('class', 'divisionBlock')
+    .attr('x', d => d.x + d.offsetX)
+    .attr('y', d => d.y + d.offsetY)
+    .attr('width', blockSize)
+    .attr('height', blockSize)
+    .attr('fill', d => d.fill);
 }
